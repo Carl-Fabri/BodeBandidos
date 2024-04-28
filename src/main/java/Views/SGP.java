c37d7c174ed441dd6ecedd55a4172f9f3158afa0
Carl-Fabri <ecefabriciof@gmail.com>
2024-04-28 00:15:13 -0500

    upload proyect

---
+++ src/main/java/Views/SGP.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Views;
+
+/**
+ *
+ * @author Carlos Fabricio
+ */
+public class SGP {
+
+
+}
